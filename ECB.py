08bad2330c3f285bc9a2c3b71b8d2368e0a275e1
TylerHBE <tylerhickersonbe@gmail.com>
2026-01-14 17:32:24 -0800

    Removed unnecessary functions

--- ECB.py
+++ ECB.py
@@ -37,35 +37,3 @@ def encrypt_ecb(filename):
     with open("output.bmp", "wb") as f:
         f.write(data[:offset])  # Write the header unchanged
         f.write(encrypted_data)  # Write the encrypted data
-
-def encrypt_ecb_text(text):
-
-
-    # initialization parameters
-    aes_key = get_random_bytes(16)
-    block_size=16
-
-
-    # Read the data we want to encrypt into memory
-    data = bytearray(text)   # make editable copy in memory
-
-
-    # ---- EDITS HERE ----
-    cipher = AES.new(aes_key, AES.MODE_ECB)
-
-    # Pad data to be multiple of block size
-    padding_byte = block_size - ((len(data)) % block_size)
-    for i in range(padding_byte):
-        data.append(padding_byte)
-
-    # Encrypt the data in 16-byte blocks
-    encrypted_data = bytearray()
-    for i in range(0, len(data), block_size):
-        block = data[i:i+block_size]
-        # Pad block if it's less than 16 bytes, although this should not happen due to previous padding
-        if len(block) < block_size:
-            block.extend([0] * (block_size - len(block)))
-        encrypted_block = cipher.encrypt(bytes(block))
-        encrypted_data.extend(encrypted_block)
-
-    return encrypted_data.decode('utf-8', errors='ignore'), aes_key
