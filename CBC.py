477eb917adbf7facb139baf224ffbe6e60efac28
TylerHBE <tylerhickersonbe@gmail.com>
2026-01-14 17:29:59 -0800

    Finalized changes to CBC

--- CBC.py
+++ CBC.py
@@ -17,7 +17,8 @@ def encrypt_cbc(filename):
 
 
     # ---- EDITS HERE ----
-    cipher = AES.new(aes_key, AES.MODE_CBC)
+    iv = get_random_bytes(16)
+    cipher = AES.new(aes_key, AES.MODE_CBC, iv)
 
     # Pad data to be multiple of block size
     padding_byte = block_size - ((len(data)-offset) % block_size)
@@ -25,17 +26,9 @@ def encrypt_cbc(filename):
         data.append(padding_byte)
 
     # Encrypt the data in 16-byte blocks
-    encrypted_data = bytearray()
-    prev_block = bytearray(os.urandom(block_size))  # Random Initialization Vector (IV)
+    encrypted_data = data[offset:]
+    encrypted_data = bytearray(cipher.encrypt(bytes(encrypted_data)))
 
-    for i in range(offset, len(data), block_size):
-        block = data[i:i+block_size]
-        block = [x ^ y for x, y in zip(block, prev_block)]
-        # Pad block if it's less than 16 bytes, although this should not happen due to previous padding
-        if len(block) < block_size:
-            block.extend([0] * (block_size - len(block)))
-        encrypted_block = cipher.encrypt(bytes(block))
-        encrypted_data.extend(encrypted_block)
 
     # Write the modified data back to a new file
     with open("output.bmp", "wb") as f:
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import CBC
+import ECB
 
 encoded_text, aes_key, iv = CBC.encrypt_cbc_text("cp-logo.bmp")
 
@@ -7,3 +8,5 @@ print("Encrypted text (CBC):", encoded_text)
 decoded_text = CBC.decrypt_cbc_text(encoded_text, aes_key, iv)
 
 print("Decrypted text (CBC):", decoded_text)
+
+CBC.encrypt_cbc("cp-logo.bmp")
