88784da4802ee966840ed7fd51b0319a8a427d9b
oledrick <oledrick@calpoly.edu>
2026-01-16 16:25:40 -0800

    Reduced the number of bit flips needed

--- bitflip.py
+++ bitflip.py
@@ -14,13 +14,13 @@ def submit(message):
 def verify(message, key, iv) -> bool:
     unencrypted_text = CBC.decrypt_cbc_text(message, key, iv)
     print(unencrypted_text)
-    if unencrypted_text.find(";admin=true") != -1:
+    if unencrypted_text.find(";admin=true;") != -1:
         return True
     return False
 
 
 def bit_flip_attack():
-    message = ";admin=true"
+    message = "0000000%admin%true%"
     text, key, iv = submit(message)
     #edit the text
     text = bytearray(text)
@@ -29,7 +29,7 @@ def bit_flip_attack():
     #take the index of which character you want to swap
     #subtract 16-1 (basically)
     # xor by (character_there ^ character_you_want)
-    # text[11] = text[11] ^ 0x79
+    #text[11] = text[11] ^ 0x79
     # text[17] = text[17] ^ 0x18
     # text[18] = text[18] ^ 0x47
     # text[19] = text[19] ^ 0x36
@@ -37,29 +37,37 @@ def bit_flip_attack():
     # text[21] = text[21] ^ 0x17
     # text[11] = text[11] ^ 0x79
 
+    #did it in 3
+    #% -> ;
+    text[16] = text[16] ^ 0x1e
+    #% -> =
+    text[22] = text[22] ^ 0x18
+    #% -> ;
+    text[27] = text[27] ^ 0x1e
+
     #worse solution because I struggled with debugging, I am only working in one block of 16 bytes
-    #%->;
-    text[17] = text[17] ^ 0x1e
-    #3->a
-    text[18] = text[18] ^ 0x52
-    #D->d
-    text[19] = text[19] ^ 0x20
-    #t->m
-    text[20] = text[20] ^ 0x19
-    #r->i
-    text[21] = text[21] ^ 0x1b
-    #u->n
-    text[22] = text[22] ^ 0x1b
-    #e->=
-    text[23] = text[23] ^ 0x58
-    #4->t
-    text[24] = text[24] ^ 0x40
-    #3->r
-    text[25] = text[25] ^ 0x41
-    #B->u
-    text[26] = text[26] ^ 0x37
-    #s->e
-    text[27] = text[27] ^ 0x16
+    # #%->;
+    #text[17] = text[17] ^ 0x1e
+    # #3->a
+    # text[18] = text[18] ^ 0x52
+    # #D->d
+    # text[19] = text[19] ^ 0x20
+    # #t->m
+    # text[20] = text[20] ^ 0x19
+    # #r->i
+    # text[21] = text[21] ^ 0x1b
+    # #u->n
+    # text[22] = text[22] ^ 0x1b
+    # #e->=
+    # text[23] = text[23] ^ 0x58
+    # #4->t
+    # text[24] = text[24] ^ 0x40
+    # #3->r
+    # text[25] = text[25] ^ 0x41
+    # #B->u
+    # text[26] = text[26] ^ 0x37
+    # #s->e
+    # text[27] = text[27] ^ 0x16
 
     print(verify(text, key, iv))
 
